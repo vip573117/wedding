a971d2bc88b867415529c9f923798aaacbac21f1
Kk50kg <485383336@qq.com>
2018-09-29 11:48:03 +0800

    Kk  9  29

--- maixun_wedding/pages/date/date.js
+++ maixun_wedding/pages/date/date.js
@@ -18,6 +18,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
+  onLoad: function (options){
     console.log(options.wid)
     var page= this
     page.setData({
@@ -63,6 +64,7 @@ Page({
       date: e.detail.value
     });
   },
+  formSubmit:function(){
     console.log(app.openid)
     var page =this
     var wid= page.data.wid 
@@ -121,7 +123,7 @@ Page({
         },
         method: 'POST',
         data: {
-          wid: e.detail.value.wid,
+          wid: wid,
           openid: app.openid,
           bride: e.detail.value.bride,
           bridephone: e.detail.value.bridephone,
@@ -164,110 +166,23 @@ Page({
         fail: function () {
           wx.hideLoading()
         }
-    var page = this
-    if (page.data.date == '') {
-      wx.showToast({
-        title: '请选择婚礼日期',
-        icon: 'none',
-        duration: 2000,
-        mask: true
       })
-      return false;
-    }
-    if (!e.detail.value.bride || e.detail.value.bride == '') {
-      wx.showToast({
-        title: '请填写新娘姓名',
-        icon: 'none',
-        duration: 2000,
-        mask: true
-      })
-      return false;
-    }
-    if (e.detail.value.bridephone == '' || !e.detail.value.bridephone) {
-      wx.showToast({
-        title: '请填写新娘电话',
-        icon: 'none',
-        duration: 2000,
-        mask: true
-      })
-      return false;
-    }
-    if (e.detail.value.groom == '' || !e.detail.value.groom) {
-      wx.showToast({
-        title: '请填写新郎姓名',
-        icon: 'none',
-        duration: 2000,
-        mask: true
-      })
-    } else {
-      app.util.request({
-        url: 'entry/wxapp/selectmanager',
-        header: {
-          'content-type': 'application/x-www-form-urlencoded'
-          //'content-type': 'application/json'
-        },
-        method: 'POST',
-        data: {
-          wid: wid
-        },
-        success: function (res) {
-          console.log(res)
-          if (res.data.code == 200) {
-            wx.showToast({
-              title: '请使用' + res.data.data + '的微信进行设置',
-              icon: 'none',
-              duration: 2000,
-              mask: true
-            })
-          } else {
-            wx.showToast({
-              title: res.data.msg,
-              icon: 'none',
-              duration: 2000,
-              mask: true
-            })
-          }
-        },
-        fail: function () {
-          wx.hideLoading()
-        }
-      })
-    }
+    }else{
     app.util.request({
-      url: 'entry/wxapp/addwedding',
+      url: 'entry/wxapp/selectmanager',
       header: {
         'content-type': 'application/x-www-form-urlencoded'
         //'content-type': 'application/json'
       },
       method: 'POST',
       data: {
-        wid: e.detail.value.wid,
-        openid: app.openid,
-        bride: e.detail.value.bride,
-        bridephone: e.detail.value.bridephone,
-        groom: e.detail.value.groom,
-        groomphone: e.detail.value.groomphone,
-        weddate: page.data.date,
-        role_type: 1
+        wid: wid,
       },
       success: function (res) {
         console.log(res)
         if (res.data.code == 200) {
           wx.showToast({
-            title: res.data.msg,
-            icon: 'none',
-            duration: 2000,
-            mask: true
-          })
-          setTimeout(function () {
-            wx.redirectTo({
-              url: '../../pages/index/index?redIndex=9'
-            });
-          }, 2000)
-        }
-        else if (res.data.code >= 201 && res.data.code <= 205) {
-          wx.showToast({
-            title: res.data.msg,
+            title: '请使用' + res.data.data+'的微信修改',
             icon: 'none',
             duration: 2000,
             mask: true
@@ -286,6 +201,7 @@ Page({
       }
 
     })
+    }
   },
   bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
